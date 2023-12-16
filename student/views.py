92f6fb2c28c4b0cf5e165910ff74a0f173b9ef3c
erfann31 <erfannasri2@gmail.com>
2023-12-16 19:05:11 +0330

    ordring-search-filter

--- student/views.py
+++ student/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Student
@@ -7,3 +9,7 @@ from .serializers import StudentSerializer
 class StudentViewSet(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    filterset_fields = '__all__'
+    ordering_fields = ['name', 'national_code', 'created_at']
+    search_fields = ['name']
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
