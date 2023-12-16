463498aec035973b23056d5770ad580aa1f52ee8
erfann31 <erfannasri2@gmail.com>
2023-12-16 19:09:48 +0330

    add calc gpa and fix urls

--- log/views.py
+++ log/views.py
@@ -1,5 +1,9 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from student.models import Student
 from .models import Log
 from .serializers import reportCardSerializer
 
@@ -7,3 +11,24 @@ from .serializers import reportCardSerializer
 class LogViewSet(ModelViewSet):
     queryset = Log.objects.all()
     serializer_class = reportCardSerializer
+
+
+@api_view(['GET'])
+def calc_gpa(request, student_id):
+    try:
+        student = Student.objects.get(pk=student_id)
+        log = Log.objects.filter(student=student)
+        curses = 0
+        total = 0
+
+        for log in log:
+            curses = 4
+            total += (log.math + log.physic + log.chemistry + log.farsi)
+
+        if curses == 0:
+            return Response({'detail': 'no log for this student'}, status=status.HTTP_404_NOT_FOUND)
+
+        gpa = total / curses
+        return Response({'student_id': student_id, 'gpa': gpa}, status=status.HTTP_201_CREATED)
+    except Student.DoesNotExist:
+        return Response({"detail": "Student doesnt exist"}, status=status.HTTP_404_NOT_FOUND)
--- ui_web_drf/urls.py
+++ ui_web_drf/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken import views
 
+from log import views as log_views
 from student import views as student_views
 
 router = routers.DefaultRouter()
 router.register(r'students', student_views.StudentViewSet)
+router.register(r'logs', log_views.LogViewSet)
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  path('calc-gpa/<int:student_id>/', log_views.calc_gpa,name='calc_gpa'),
+                  path('api-token-auth/', views.obtain_auth_token),
+                  path('api-auth/', include('rest_framework.urls')),
               ] + router.urls
