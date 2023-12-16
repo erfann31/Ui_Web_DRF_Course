465d082f930df42e4b1e979ed623b858fe4d96e2
erfann31 <erfannasri2@gmail.com>
2023-12-16 19:05:33 +0330

    pagination

--- student/views.py
+++ student/views.py
@@ -1,10 +1,15 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
 from .models import Student
 from .serializers import StudentSerializer
 
+class CustomPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 20
 
 class StudentViewSet(ModelViewSet):
     queryset = Student.objects.all()
@@ -13,3 +18,4 @@ class StudentViewSet(ModelViewSet):
     ordering_fields = ['name', 'national_code', 'created_at']
     search_fields = ['name']
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    pagination_class = CustomPagination
